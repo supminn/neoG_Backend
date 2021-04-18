40b7dbf64c19b7f8dd8a54a7ec2a1531df5b00ba
supminn <30731236+supminn@users.noreply.github.com>
2021-04-18 19:17:17 +0530

    fix: DB connect - issue

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   try {
-    const response = await mongoose.connect(dbURI, {
+    const response = await mongoose.connect("mongodb+srv://supminn:db4n3O!p@sup-cluster.fu7o7.mongodb.net/inventory?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
