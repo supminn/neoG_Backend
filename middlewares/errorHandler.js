208b259a56c4969e63fa738528c286243bf1ee4b
supminn <30731236+supminn@users.noreply.github.com>
2021-04-26 10:10:35 +0530

    creating git repo

--- middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ success: false, message: err.message });
-};
-
-module.exports = errorHandler;
--- middlewares/routeHandler.js
+++
@@ -1,8 +0,0 @@
-const routeHandler = (req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "The route you're looking for couldn't be found",
-  });
-};
-
-module.exports = routeHandler;
